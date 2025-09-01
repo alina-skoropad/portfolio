d25d6e288205f27b13d312c28dda2dbb7a7727be
Alina Skoropad <alina@jaybirdgroup.com>
2025-09-01 18:22:21 +0300

    About copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.main__title}>
-          Hello! I'm Alina<span>, a Web Designer and an HTML/CSS developer</span>. I have a passion for creating clean, user-friendly, and timeless digital experiences that really work. My focus on minimalism and simplicity allows me to design solutions that are both elegant and functional.
+          Hello! I'm Alina<span>, a Web Designer and an HTML/CSS developer</span>. I am passionate about both design and development. I strive to create clean, user-friendly, and timeless digital experiences that truly work. My focus on minimalism and simplicity enables me to design solutions that are both elegant and functional.
         </h1>
         <div className={styles.main__projects}>
           <h3 className={styles.main__subtitle}>recent works</h3>
