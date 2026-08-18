76ac7522eab2ebaa3e6f463b43d1e9dd09e00da1
Alina <alina@jaybirdgroup.com>
2026-08-18 16:03:18 +0300

    Fixes

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -58,20 +58,6 @@ const Projects = ({ activeFilter, onSelectFilter }: ProjectsProps) => {
                 </div>
                 <h2>
                   {project.title}
-                  <span className={styles.arrow_wrapper}>
-                    <svg
-                      className={styles.arrow}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="0.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <line x1="7" y1="17" x2="17" y2="7" />
-                      <polyline points="7 7 17 7 17 17" />
-                    </svg>
-                  </span>
                 </h2>
               </Link>
 
